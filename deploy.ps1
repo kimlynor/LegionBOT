a7281ab1469622f47b844d397a8584f8ef5e46dd
aeonian <aeonian@071labs.io>
2026-03-06 16:14:45 +0900

    서버 자동적용 이름변경

--- deploy.ps1
+++
@@ -1,7 +0,0 @@
-$OutputEncoding = [System.Text.Encoding]::UTF8
-[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-git add .
-$msg = Read-Host "커밋 메시지 입력"
-git commit -m $msg
-git push origin main
-ssh ubuntu@192.168.71.90 "cd /home/ubuntu/legionbot && git pull && sudo systemctl restart legionbot && sudo systemctl status legionbot --no-pager"
