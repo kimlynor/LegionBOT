677a7d2e54c5b78a7da74f74278717e155abecb7
aeonian <aeonian@071labs.io>
2026-03-06 16:06:59 +0900

    테스트 커밋

---
+++ deploy.ps1
@@ -0,0 +1,5 @@
+git add .
+$msg = Read-Host "커밋 메시지 입력"
+git commit -m $msg
+git push origin main
+ssh ubuntu@192.168.71.90 "cd /home/ubuntu/legionbot && git pull && sudo systemctl restart legionbot && sudo systemctl status legionbot --no-pager"
